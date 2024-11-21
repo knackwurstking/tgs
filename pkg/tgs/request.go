d0d36b346c54823779543e0f329e86929977609a
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-21 12:44:13 +0100

    Adding get/post methods & todos

--- pkg/tgs/request.go
+++ pkg/tgs/request.go
@@ -1,14 +1,60 @@
 package tgs
 
-type RequestGetMe struct{}
+import "fmt"
+
+type GetRequest interface {
+	Token
+	Get() (any, error)
+}
+
+type PostRequest interface {
+	Token
+	Post() (any, error)
+}
+
+type Token struct {
+	token string
+}
+
+func (t *Token) Token() string {
+	return t.token
+}
+
+func (t *Token) SetToken(token string) {
+	t.token = token
+}
+
+type RequestGetMe struct {
+	Token
+}
+
+func (r *RequestGetMe) Get() (*ResponseGetMe, error) {
+	if r.token == "" {
+		return nil, fmt.Errorf("missing token")
+	}
+
+	// TODO: ...
+
+	return nil, fmt.Errorf("under construction")
+}
 
 type RequestGetUpdates struct {
-	Offset *int `json:"offset"`
-	// Limit defaults to 100
-	Limit *int `json:"limit"`
-	// Timeout defaults to 0 (Short Polling)
-	Timeout        *int     `json:"timeout"`
+	Token
+
+	Offset         *int     `json:"offset"`
+	Limit          *int     `json:"limit"`   // Limit defaults to 100
+	Timeout        *int     `json:"timeout"` // Timeout defaults to 0 (Short Polling)
 	AllowedUpdates []string `json:"allowed_updates"`
+}
+
+func (r *RequestGetUpdates) Get() (*ResponseGetUpdates, error) {
+	if r.token == "" {
+		return nil, fmt.Errorf("missing token")
+	}
+
+	// TODO: ...
+
+	return nil, fmt.Errorf("under construction")
 }
 
 // TODO: "setMyCommands"
