e03cfefcd599511712dac0c3852b17056799d1d3
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-23 14:53:58 +0100

    Update todos - main

--- cmd/tgs-server/main.go
+++ cmd/tgs-server/main.go
@@ -1,1 +1,12 @@
 package main
+
+func main() {
+	config := NewConfig()
+	loadConfig(config)
+
+	// TODO: Check token, register commands, enter the main loop and wait for updates
+}
+
+func loadConfig(config *Config) {
+	// TODO: Load YAML configuration
+}
