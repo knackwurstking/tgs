121ba8d0e3d5bfd4607e4521d2a3373446c7348e
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-23 14:51:18 +0100

    Update server config structs (bot commands)

--- cmd/tgs-server/config.go
+++ cmd/tgs-server/config.go
@@ -4,10 +4,23 @@ import (
 	"github.com/knackwurstking/tgs/pkg/tgs"
 )
 
+type Users []*tgs.User
+type Chats []*tgs.Chat
+
 type Config struct {
 	Token    string      `json:"token" yaml:"token"`
 	Targets  Targets     `json:"targets" yaml:"targets"`
 	Commands BotCommands `json:"commands" yaml:"commands"`
+}
+
+func NewConfig() *Config {
+	return &Config{
+		Targets: Targets{
+			Users: make(Users, 0),
+			Chats: make(Chats, 0),
+		},
+		Commands: BotCommands{},
+	}
 }
 
 type Targets struct {
@@ -15,12 +28,17 @@ type Targets struct {
 	Chats Chats `json:"chats" yaml:"chats"`
 }
 
-type Users []*tgs.User
-
-type Chats []*tgs.Chat
-
-type BotCommands []BotCommand
+type BotCommands struct {
+	IP          BotCommand `json:"ip" yaml:"ip"`
+	JournalList BotCommand `json:"journallist" yaml:"journallist"`
+	Journal     BotCommand `json:"journal" yaml:"journal"`
+	PicowStatus BotCommand `json:"picowstatus" yaml:"picowstatus"`
+	PicowOn     BotCommand `json:"picowon" yaml:"picowon"`
+	PicowOff    BotCommand `json:"picowoff" yaml:"picowoff"`
+	OPManga     BotCommand `json:"opmanga" yaml:"opmanga"`
+	OPMangaList BotCommand `json:"opmangalist" yaml:"opmangalist"`
+}
 
 type BotCommand struct {
-	// TODO: ...
+	Targets *Targets `json:"targets" yaml:"targets"`
 }
