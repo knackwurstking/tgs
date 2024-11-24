aeead0fbde70b74f8ff08fdbfa29221494318424
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-24 14:36:20 +0100

    Add nice cli

--- cmd/tgs-server/main.go
+++ cmd/tgs-server/main.go
@@ -1,12 +1,46 @@
 package main
 
-func main() {
-	config := NewConfig()
-	loadConfig(config)
+import (
+	"fmt"
 
-	// TODO: Check token, register commands, enter the main loop and wait for updates
+	"github.com/SuperPaintman/nice/cli"
+)
+
+func main() {
+	app := cli.App{
+		Name:  "tgs-server",
+		Usage: cli.Usage("Telegram scripts server, the scripts part was kicked from the project :)"),
+		Action: cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
+			var configPath string
+
+			cli.StringVar(cmd, &configPath, "config",
+				cli.Usage("Path to server configuration (yaml)"),
+				cli.Required,
+			)
+
+			return func(cmd *cli.Command) error {
+				config := NewConfig()
+
+				if err := loadConfig(config, configPath); err != nil {
+					return err
+				}
+
+				// TODO: Check token, register commands, enter the main loop and wait for updates
+
+				return nil
+			}
+		}),
+		CommandFlags: []cli.CommandFlag{
+			cli.HelpCommandFlag(),
+			cli.VersionCommandFlag("0.0.0"),
+		},
+	}
+
+	app.HandleError(app.Run())
 }
 
-func loadConfig(config *Config) {
+func loadConfig(config *Config, path string) error {
 	// TODO: Load YAML configuration
+
+	return fmt.Errorf("under construction")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/knackwurstking/tgs
 
 go 1.23
+
+require github.com/SuperPaintman/nice v0.0.0-20211001214957-a29cd3367b17
+
+require (
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
+)
