c273d1ec1cf29e6d7df7427e608320095748ac57
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-24 19:50:30 +0100

    Update chats and users types and add Get method to CommandConfigs

--- cmd/tgs-server/config.go
+++ cmd/tgs-server/config.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/knackwurstking/tgs/pkg/data"
+import (
+	"fmt"
 
-type Users []*data.User
-type Chats []*data.Chat
+	"github.com/knackwurstking/tgs/pkg/data"
+)
+
+type Users []data.User
+type Chats []data.Chat
 
 type Config struct {
 	Token    string         `json:"token" yaml:"token"`
@@ -35,6 +39,29 @@ type CommandConfigs struct {
 	PicowOff    CommandConfig `json:"picowoff" yaml:"picowoff"`
 	OPManga     CommandConfig `json:"opmanga" yaml:"opmanga"`
 	OPMangaList CommandConfig `json:"opmangalist" yaml:"opmangalist"`
+}
+
+func (c *CommandConfigs) Get(command string) (*CommandConfig, error) {
+	switch command {
+	case "/ip":
+		return &c.IP, nil
+	case "/journallist":
+		return &c.JournalList, nil
+	case "/journal":
+		return &c.Journal, nil
+	case "/picowstatus":
+		return &c.PicowStatus, nil
+	case "/picowon":
+		return &c.PicowOn, nil
+	case "/picowoff":
+		return &c.PicowOff, nil
+	case "/opmanga":
+		return &c.OPManga, nil
+	case "/opmangalist":
+		return &c.OPMangaList, nil
+	default:
+		return nil, fmt.Errorf("command %s not found", command)
+	}
 }
 
 type CommandConfig struct {
