429993c93157677f6903b0137b7c0503e8536e0c
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-28 17:48:25 +0100

    Add stats (bot command) & TODO

--- internal/bot-commands/stats/stats.go
+++ internal/bot-commands/stats/stats.go
@@ -1,3 +1,25 @@
 package stats
 
+import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+func New(botAPI *tgbotapi.BotAPI) *Stats {
+	return NewStats(botAPI)
+}
+
+type Stats struct {
+	*tgbotapi.BotAPI
+}
+
+func NewStats(botAPI *tgbotapi.BotAPI) *Stats {
+	return &Stats{
+		BotAPI: botAPI,
+	}
+}
+
 // TODO: Deliver user stats like user id (+username), chat id (+title), topic number (message_thread_id, +reply_to_message.forum_topic_created.name)
+
+func (this *Stats) Run(message *tgbotapi.Message) error {
+	// TODO: Continue here...
+
+	return nil
+}
