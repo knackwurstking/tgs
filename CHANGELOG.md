4dd99f7707a5f1691f13e8de0431361a27e72bf0
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-29 19:49:28 +0100

    Set v0.2.0.dev

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # CHANGELOG
 
+## v0.2.0 [unreleased]
+
 ## v0.1.0 [2024-11-29]
 
 - Added “/ip” and “/stats” bot commands.
--- cmd/tgs-server/main.go
+++ cmd/tgs-server/main.go
@@ -127,7 +127,7 @@ func main() {
 		}),
 		CommandFlags: []cli.CommandFlag{
 			cli.HelpCommandFlag(),
-			cli.VersionCommandFlag("0.1.0"),
+			cli.VersionCommandFlag("0.2.0.dev"),
 		},
 	}
 
