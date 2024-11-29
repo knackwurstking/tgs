d1e22d7ece437b0daa47004c456a9236297c6f51
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-29 19:06:22 +0100

    Add user_id to stats

--- internal/botcommand/stats.go
+++ internal/botcommand/stats.go
@@ -20,10 +20,12 @@ func NewStats(botAPI *tgbotapi.BotAPI) *Stats {
 func (this *Stats) Run(message *tgbotapi.Message) error {
 	data := struct {
 		UserName        string `json:"username"`
+		UserID          int64  `json:"user_id"`
 		ChatID          int64  `json:"chat_id"`
 		MessageThreadID int    `json:"message_thread_id"`
 	}{
 		UserName:        message.From.UserName,
+		UserID:          message.From.ID,
 		ChatID:          message.Chat.ID,
 		MessageThreadID: message.MessageThreadID,
 	}
