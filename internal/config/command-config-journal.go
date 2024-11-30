eaed88d885fb617620629ce8462443e42aa7f93e
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-30 06:13:21 +0100

    Update journal get method

--- internal/config/command-config-journal.go
+++ internal/config/command-config-journal.go
@@ -6,6 +6,7 @@ import (
 	"github.com/knackwurstking/tgs/pkg/tgs"
 )
 
+// TODO: Add Units ("units") field
 type CommandConfigJournal struct {
 	Register          []tgs.BotCommandScope `json:"register" yaml:"register"`
 	ValidationTargets *ValidationTargets    `json:"targets" yaml:"targets"`
@@ -23,17 +24,63 @@ type Units struct {
 	User   []string `json:"user" yaml:"user"`
 }
 
-func (this *Units) Get(unit string, fromUser bool) (data []byte, err error) {
-	data = make([]byte, 0)
+func NewUnits() *Units {
+	return &Units{
+		System: make([]string, 0),
+		User:   make([]string, 0),
+	}
+}
 
+func (this *Units) Has(unit string) bool {
 	units := make([]string, 0)
-	if fromUser {
-		units = append(units, this.User...)
-	} else {
-		units = append(units, this.System...)
+	units = append(units, this.System...)
+	units = append(units, this.User...)
+	for _, u := range this.System {
+		if u == unit {
+			return true
+		}
 	}
 
-	// TODO: Get journal from unit from user or system
+	return false
+}
+
+func (this *Units) IsSystemUnit(unit string) bool {
+	for _, u := range this.System {
+		if u == unit {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (this *Units) IsUserUnit(unit string) bool {
+	for _, u := range this.User {
+		if u == unit {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (this *Units) Get(unit string) (data []byte, err error) {
+	var isUser bool
+	if this.IsUserUnit(unit) {
+		isUser = true
+	} else if !this.IsSystemUnit(unit) {
+		return nil, fmt.Errorf("missing unit: %s", unit)
+	}
+
+	data = make([]byte, 0)
+
+	if isUser {
+		// TODO: Create user command here for journal
+	} else {
+		// TODO: Create system command here for journal
+	}
+
+	// TODO: Run command here
 
 	return nil, fmt.Errorf("under construction")
 }
