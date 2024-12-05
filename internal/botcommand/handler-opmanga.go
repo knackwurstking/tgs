fbc187822deccf4b45103287bd0f3bc250507268
Udo Bauer <knackwurstking.tux@gmail.com>
2024-12-06 00:55:31 +0100

    Remove log

--- internal/botcommand/handler-opmanga.go
+++ internal/botcommand/handler-opmanga.go
@@ -3,7 +3,6 @@ package botcommand
 import (
 	"encoding/json"
 	"fmt"
-	"log/slog"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -166,7 +165,6 @@ func (this *OPManga) handleListCommand(m *tgbotapi.Message) error {
 	})
 	documentConfig.ReplyToMessageID = m.MessageID
 
-	slog.Debug("Send template with data", "content", string(content), "documentConfig", documentConfig, "bot", this.BotAPI)
 	_, err = this.BotAPI.Send(documentConfig)
 	return err
 }
