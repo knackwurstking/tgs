ff9ff9b5b471063cc79be7e64e14325689936cfe
Udo Bauer <knackwurstking.tux@gmail.com>
2025-01-01 20:03:21 +0100

    Update template patterns

--- internal/botcommand/journal/template-data.go
+++ internal/botcommand/journal/template-data.go
@@ -10,10 +10,7 @@ func (td *TemplateData) Patterns() []string {
 	return []string{
 		"data/index.go.html",
 		"data/journallist.go.html", // block: content
-		//"data/pico.min.css", // block: style
-		"data/ui.min.css",   // block: style
-		"data/original.css", // block: theme
-		"data/styles.css",   // block: custom-style
-		//"data/ui.min.umd.cjs", // block: script
+		"data/ui.css",              // block: ui
+		"data/styles.css",          // block: styles
 	}
 }
