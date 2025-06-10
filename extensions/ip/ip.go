afe12df5194c33317a3ab91056f3eaf20866a408
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-10 16:41:55 +0200

    Move the old IP botcommand struct to extensions

--- extensions/ip/ip.go
+++ extensions/ip/ip.go
@@ -1,17 +1,141 @@
 package ip
 
-import "github.com/knackwurstking/tgs/pkg/extension"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"gopkg.in/yaml.v3"
+
+	"github.com/knackwurstking/tgs/internal/botcommand"
+	"github.com/knackwurstking/tgs/pkg/extension"
+	"github.com/knackwurstking/tgs/pkg/tgs"
+)
+
+const (
+	IP4URL = "https://ifconfig.io"
+	IP6URL = "https://ipv6.icanhazip.com"
+)
+
+type Data struct {
+	Targets  *botcommand.Targets   `yaml:"targets,omitempty"`
+	Register []tgs.BotCommandScope `yaml:"register,omitempty"`
+}
 
 type IP struct {
-	extension.Extension
+	*tgbotapi.BotAPI
+
+	data *Data
 }
 
-func NewIP() *IP {
-	return &IP{}
+func NewIP(api *tgbotapi.BotAPI) *IP {
+	return &IP{
+		BotAPI: api,
+		data: &Data{
+			Targets:  botcommand.NewTargets(),
+			Register: make([]tgs.BotCommandScope, 0),
+		},
+	}
 }
 
-func NewIPExtension() extension.Extension {
-	return NewIP()
+func NewIPExtension(api *tgbotapi.BotAPI) extension.Extension {
+	return NewIP(api)
 }
 
-// TODO: Move stuff from `internal/botcommand/ip` to this package here
+func (ip *IP) ConfigPath() string {
+	return "ip.config"
+}
+
+func (ip *IP) MarshalYAML() (any, error) {
+	return ip.data, nil
+}
+
+func (ip *IP) UnmarshalYAML(value *yaml.Node) error {
+	if err := value.Decode(ip.data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ip *IP) Register() []tgs.BotCommandScope {
+	return ip.data.Register
+}
+
+func (ip *IP) Targets() *botcommand.Targets {
+	return ip.data.Targets
+}
+
+func (ip *IP) Commands(mbc *tgs.MyBotCommands) {
+	mbc.Add("/ip", "Get server IP", ip.Register())
+}
+
+func (ip *IP) GetIPv4AddressFromURL() (address string, err error) {
+	resp, err := http.Get(IP4URL)
+	if err != nil {
+		return address, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return address, fmt.Errorf("request to %s: %d (%s)",
+			IP4URL, resp.StatusCode, resp.Status,
+		)
+	}
+
+	defer resp.Body.Close()
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return address, err
+	}
+
+	return strings.Trim(string(data), "\n\r\t "), nil
+}
+
+func (ip *IP) GetIPv6AddressFromURL() (address string, err error) {
+	resp, err := http.Get(IP6URL)
+	if err != nil {
+		return address, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return address, fmt.Errorf("request to %s: %d (%s)",
+			IP6URL, resp.StatusCode, resp.Status,
+		)
+	}
+
+	defer resp.Body.Close()
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return address, err
+	}
+
+	return strings.Trim(string(data), "\n\r\t "), nil
+}
+
+func (ip *IP) Handler(message *tgbotapi.Message) error {
+	if command := message.Command(); command != "ip" {
+		return fmt.Errorf("unknown command: %s", command)
+	}
+
+	ipv4, err := ip.GetIPv4AddressFromURL()
+	if err != nil {
+		ipv4 = err.Error()
+	}
+
+	ipv6, err := ip.GetIPv6AddressFromURL()
+	if err != nil {
+		ipv6 = err.Error()
+	}
+
+	msgConfig := tgbotapi.NewMessage(
+		message.Chat.ID,
+		fmt.Sprintf(
+			"**IPv4**: `%s`\n**IPv6**: `%s`", ipv4, ipv6,
+		),
+	)
+	msgConfig.ReplyToMessageID = message.MessageID
+	msgConfig.ParseMode = "MarkdownV2"
+
+	_, err = ip.Send(msgConfig)
+	return err
+}
--- pkg/extension/extension.go
+++ pkg/extension/extension.go
@@ -1,4 +1,19 @@
 package extension
 
-// NOTE: `any` is just a placeholder
-type Extension any
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"gopkg.in/yaml.v3"
+
+	"github.com/knackwurstking/tgs/internal/botcommand"
+	"github.com/knackwurstking/tgs/pkg/tgs"
+)
+
+type Extension interface {
+	ConfigPath() string
+	MarshalYAML() (any, error)
+	UnmarshalYAML(value *yaml.Node) error
+	Register() []tgs.BotCommandScope
+	Targets() *botcommand.Targets
+	Commands(mbc *tgs.MyBotCommands)
+	Handler(message *tgbotapi.Message) error
+}
