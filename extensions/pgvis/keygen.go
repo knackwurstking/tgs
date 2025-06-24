f8af20c1e97034e6ebb7e820cc1123de99b0b8e6
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-24 22:07:37 +0200

    update todos

--- extensions/pgvis/keygen.go
+++ extensions/pgvis/keygen.go
@@ -1,10 +1,5 @@
 package pgvis
 
-import (
-	"database/sql"
-	"errors"
-)
-
 type User struct {
 	ID       int64  `json:"id"`
 	UserName string `json:"user_name"`
@@ -12,18 +7,14 @@ type User struct {
 }
 
 func NewUser(id int64, userName string) *User {
+	u := &User{
+		ID:       id,
+		UserName: userName,
+		ApiKey:   "",
+	}
+
 	// TODO: Generate api key for user (id), but check database first, return existing user if available
 	apiKey := "<api-key>"
 
-	return &User{
-		ID:       id,
-		UserName: userName,
-		ApiKey:   apiKey,
-	}
-}
-
-func createUser(db *sql.DB, string, user *User) error {
-	// TODO: Create a new user inside the pg vis server database
-
-	return errors.New("under construction")
+	return u
 }
