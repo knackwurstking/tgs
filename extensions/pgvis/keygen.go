3a15acab62e3812e45415a8c9abdc5dd68fab87e
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-27 17:57:52 +0200

    add comments

--- extensions/pgvis/keygen.go
+++ extensions/pgvis/keygen.go
@@ -23,22 +23,28 @@ func NewUser(id int64, userName string) (*User, error) {
 		ApiKey:   "",
 	}
 
+	// Get api key for this user (telegram id), if possible
 	cmd := exec.Command("pg-vis", "user", "show", "--api-key", fmt.Sprintf("%d", id))
+
 	if err := cmd.Run(); err != nil {
 		if c, ok := err.(*exec.ExitError); !ok {
+			// Command failed
 			return u, err
 		} else {
 			slog.Debug(fmt.Sprintf("Command failed with %d", c.ExitCode()))
 
-			// NOTE: For now, 10 is the exit code in use for not found
+			// NOTE: For now, 10 is the code used for not found
 			if c.ExitCode() != PGVisExitCodeNotFound {
 				return u, fmt.Errorf(
 					"pg-vis command failed with an exit code %d",
 					c.ExitCode(),
 				)
+			} else {
+				// TODO: Create user, using the pg-vis command here
 			}
 		}
 	} else {
+		// Get the api-key from the command output
 		if apiKey, err := cmd.Output(); err != nil {
 			return u, fmt.Errorf("output error: %s", err.Error())
 		} else {
@@ -46,16 +52,17 @@ func NewUser(id int64, userName string) (*User, error) {
 		}
 	}
 
+	// If the user has no api-key, create a new one one
 	if u.ApiKey == "" {
 		u.ApiKey = generateApiKey()
+		// TODO: Update user, using the pg-vis command here
 	}
 
 	return u, nil
 }
 
 func generateApiKey() string {
-	// TODO: Generate a new api key, use the pg-vis command for this, and
-	// 		 create the user
+	// TODO: Generate a new api key, use the pg-vis command for this
 
 	return "<api-key>"
 }
